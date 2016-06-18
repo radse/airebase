de1524cdcf38ff8b0802bd52e8332acd66fe4371
radse <radse@outlook.com>
2016-06-18 14:02:17 -0300

    Creacion de las acciones new y create con sus respectivas vistas

--- app/controllers/aires_controller.rb
+++ app/controllers/aires_controller.rb
@@ -1,4 +1,16 @@
 class AiresController < ApplicationController
   def index
   end
+
+  def new
+    @aire = Aire.new
+  end
+
+  def create
+    @aire = Aire.new(params[:aire])
+  end
+
+  
+
+
 end
