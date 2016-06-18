9a2b7f5306225e03a2f175a5ea47fd2a5485a549
radse <radse@outlook.com>
2016-06-18 18:21:24 -0300

    Se crearon 2 fichas de aires se añadió más funcionalidad al Inde y se crearon las acciones Show con sus paginas de vistas

--- app/controllers/aires_controller.rb
+++ app/controllers/aires_controller.rb
@@ -1,5 +1,10 @@
 class AiresController < ApplicationController
+  before_action :find_aire, only: [:show, :edit, :update, :destroy]
   def index
+    @aires = Aire.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -7,10 +12,23 @@ class AiresController < ApplicationController
   end
 
   def create
-    @aire = Aire.new(params[:aire])
+    @aire = Aire.new(aire_params)
+    if @aire.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
-  
+  private
+
+  def aire_params
+    params.require(:aire).permit(:marca, :modelo_conjunto, :modelo_ui, :modelo_ue)
+  end
+
+  def find_aire
+    @aire = Aire.find(params[:id])
+  end
 
 
 end
